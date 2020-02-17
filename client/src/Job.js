f84e9b57cac6987f329a5efe48a1c4d813326ba5
Sabrina Markon <phpsitescripts@outlook.com>
2020-02-16 20:27:01 -0700

    Add basic styling to Job rows.

--- client/src/Job.js
+++ client/src/Job.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default function Job({job}) {
   return (
     <div className="job">
-      {job.title}
-      {job.company}
+      <div>{job.title}</div>
+      <div>{job.company}</div>
     </div>
   )
 }
