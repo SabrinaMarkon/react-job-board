a601756409129ba61a298a569600a262bd9088a8
Sabrina Markon <phpsitescripts@outlook.com>
2020-02-17 00:57:11 -0700

    Add Redis storage to fetch-github.js task.

--- worker/tasks/fetch-github.js
+++ worker/tasks/fetch-github.js
@@ -1,10 +1,17 @@
 // node-fetch gives node the fetch method the same as client-side fetch.
 const fetch = require('node-fetch');
+const redis = require('redis');
+const client = redis.createClient();
+
+const { promisify } = require("util");
+const getAsync = promisify(client.get).bind(client); // Get jobs from redis.
+const setAsync = promisify(client.set).bind(client); // Add jobs to redis.
 
 // With no query string parameters, the default is the first 50 jobs.
 const MAIN_API_URL = 'https://jobs.github.com/positions.json';
 
 async function fetchGitHub() {
+  console.log('Fetching GitHub jobs...');
   let currentPageJobCount, pageWeAreOn = 0, totalJobCount = 0;
   const allJobs = [];
   
@@ -20,8 +27,10 @@ async function fetchGitHub() {
   } while (currentPageJobCount === 50);
 
   console.log('got total', allJobs.length, 'jobs');
+  const success = await setAsync('github', JSON.stringify(allJobs));
+  console.log(success);
 }
 
-fetchGitHub();
+// fetchGitHub();
 
 module.exports = fetchGitHub;
