1f0052703b814f11ebf47e8e5a9b137b1a3a8f71
Sabrina Markon <phpsitescripts@outlook.com>
2020-02-16 23:40:25 -0700

    Add fetchGitHub function cronjob.

--- worker/index.js
+++ worker/index.js
@@ -1,6 +1,11 @@
-var CronJob = require('cron').CronJob;
-var job = new CronJob('* * * * * *', function() {
-  console.log('You will see this message every second');
-  // Function will be here to fetch the job ads to populate the app.
-}, null, true, 'America/Edmonton');
+// CronJob: seconds(0-59) minutes(0-59) hours(0-23) dayofmonth(1-31) month(0-11) dayofweek(0-6)
+const CronJob = require('cron').CronJob;
+
+// Functions to execute to fetch jobs from APIs:
+const fetchGitHub = require('./tasks/fetch-github');
+// const fetchIndeed = '';
+// const fetchStackOverflow = '';
+
+// All cronjobs can run in parallel and are independent of one another.
+const job = new CronJob('0 */5 * * * *', fetchGitHub, null, true, 'America/Edmonton');
 job.start();
