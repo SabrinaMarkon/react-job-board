24e201880f5745d0cf67e9b8c42d40c5a96a28fc
Sabrina Markon <phpsitescripts@outlook.com>
2020-02-29 22:07:00 -0700

    Add function and hooks to fetch jobs from API.

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Jobs from './Jobs';
 
-const mockJobs = [
-  {title: 'Front-end Developer', company: 'Google'},
-  {title: 'Java Developer', company: 'Microsoft'},
-  {title: 'UI/UX Designer', company: 'GitHub'}
-];
+const JOB_API_URL = 'http://localhost:3001/jobs';
+
+// const mockJobs = [
+//   {title: 'Front-end Developer', company: 'Google'},
+//   {title: 'Java Developer', company: 'Microsoft'},
+//   {title: 'UI/UX Designer', company: 'GitHub'}
+// ];
 
 // App component will hold state.
 function App() {
+
+  const [jobsList, setJobsList] = useState([]);
+
+  async function fetchJobs(newJobsList) {
+    const response = await fetch(JOB_API_URL);
+    const json = await response.json();
+    console.log(json);
+    setJobsList(newJobsList);
+  }
+
+  useEffect((jobsList) => {
+    fetchJobs(jobsList);
+  }, []);
+
   return (
     <div className="App">
-      <Jobs jobs={mockJobs} />
+      <Jobs jobs={jobsList} />
     </div>
   );
 }
